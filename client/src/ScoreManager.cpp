39014293f2a194601a56f2982fa27e08b2a137d1
ziw261 <ziw261@ucsd.edu>
2020-06-03 23:47:07 -0700

    refactor coin_size

--- client/src/ScoreManager.cpp
+++ client/src/ScoreManager.cpp
@@ -15,7 +15,7 @@ ScoreManager::ScoreManager(){
 void ScoreManager::UpdateScoreYCorrd(Terrain* terrain){
     //std::cout << "Get Called" << std::endl;
     for(int i=0; i<this->scoreStatus.size(); i++){
-        this->scoreStatus[i].y = terrain->getHeight(this->scoreStatus[i].x*1, this->scoreStatus[i].z*-1) + 2.5f;
+        this->scoreStatus[i].y = terrain->getHeight(this->scoreStatus[i].x*1, this->scoreStatus[i].z*-1) + COIN_SIZE/2;
     }
 
     // Debug Message
